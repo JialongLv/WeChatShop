72eecc570c7f9d128805ab995ad666871d6368e2
JialongLv <872044996@qq.com>
2017-08-18 12:24:17 +0800

    11.9

---
+++ ptotoss/utils/base.js
@@ -0,0 +1,41 @@
+
+import { Config } from '../utils/config.js';
+
+
+class Base{
+    constructor(){
+        this.baseRequestUrl = Config.resUrl;
+    }
+
+    request(params){
+        var url = this.baseRequestUrl + params.url;
+
+    if(!params.type)
+    {
+        params.type = 'GET';
+    }
+
+        wx.request({
+            url: url,
+            data: params.data,
+            header: {
+                'content-type':'application/json',
+                'token':wx.getStorageSync('token')
+            },
+            method: params.type,
+            success: function(res) {
+                // if(params.sCallBack){
+                //     params.sCallBack(res);
+                // }
+                params.sCallback&&params.sCallback(res.data);
+
+            },
+            fail: function(res) {
+                console.log(err);
+            },
+            
+        })
+    }
+}
+
+export {Base};
---
+++ ptotoss/utils/config.js
@@ -0,0 +1,10 @@
+
+class Config {
+    constructor() {
+
+    }
+}
+
+Config.resUrl = 'http://w.cn./api/v1/';
+
+export { Config };
